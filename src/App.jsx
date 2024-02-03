e668e4f626c99eaa2f7972602e994033a3821a7a
DebasishKisan1 <debasish.kisan@gmail.com>
2024-02-03 16:28:44 +0530

    About page , routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-// import './App.css'
-
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import About from "./components/About"
 import Home from "./components/layout/Home"
 import EcomctxProvider from "./store/EcomctxProvider"
+
+const router = createBrowserRouter([
+  { path: '/', element: <Home /> },
+  { path: '/about', element: <About /> }
+])
 
 function App() {
 
   return (
     <EcomctxProvider>
-      <Home />
+      <RouterProvider router={router} />
+      {/* <Home />
+      <About /> */}
     </EcomctxProvider>
   )
 }
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,14 +1,15 @@
+import { NavLink } from "react-router-dom";
 import CartButton from "./CartButton";
 
 const Navigation = (props) => {
     return (
         <div className="container-fluid bg-dark p-2 position-fixed top-0">
             <ul className="list-inline text-white text-center">
-                <li className="list-inline-item px-4">HOME</li>
-                <li className="list-inline-item px-4">STORE</li>
-                <li className="list-inline-item px-4">ABOUT</li>
+                <li className="list-inline-item px-4"><NavLink to={'/'} className='text-white nav-link fw-bold'>HOME</NavLink></li>
+                <li className="list-inline-item px-4"><NavLink to={'/store'} className='text-white nav-link fw-bold'>STORE</NavLink></li>
+                <li className="list-inline-item px-4"><NavLink to={'/about'} className='text-white nav-link fw-bold'>ABOUT</NavLink></li>
             </ul>
-            <CartButton cartHandler={props.cartHandler}/>
+            <CartButton cartHandler={props.cartHandler} />
         </div>
     )
 }
